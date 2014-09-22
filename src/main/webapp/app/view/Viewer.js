9019fd7b8a09efeb7da3478a61a7ca7ec397ee28
金为乐 <wl.jin@jiangtai.com>
2014-09-22 13:03:50 +0800

    update tab panel bug

--- src/main/webapp/app/view/Viewer.js
+++ src/main/webapp/app/view/Viewer.js
@@ -1,17 +1,17 @@
 Ext.define('CT.view.Viewer', {
     extend: 'Ext.tab.Panel',
     alias: 'widget.viewer',
+    requires: [],
+    activeItem: 0,
+    margins: '5 5 5 5',
+    cls: 'preview',
     deferredRender: false,
     activeTab: 0,     // first tab initially active
     items: [{
-        contentEl: 'center1',
-        title: 'Close Me',
-        closable: true,
-        autoScroll: true
-    }, {
-        contentEl: 'center2',
-        title: 'Center Panel',
-        autoScroll: true
+        title: '使用说明',
+        //closable: true,
+        autoScroll: true,
+        html: '欢迎使用代码生成工具，本系统正在开发中！'
     }],
     initComponent: function() {
     	
