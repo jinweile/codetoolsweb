07ccb26219052757c0f54dcda0540c5e1fbf0a0b
金为乐 <wl.jin@jiangtai.com>
2014-09-22 17:07:51 +0800

    增加toolbar

--- src/main/webapp/app/view/center/Edit.js
+++ src/main/webapp/app/view/center/Edit.js
@@ -2,14 +2,20 @@
  * 
  */
 Ext.define('CT.view.center.Edit', {
-	extend: 'Ext.panel.Panel',
-	alias: 'widget.centeredit',
-	layout: 'fit',
+	extend : 'Ext.panel.Panel',
+	alias : 'widget.centeredit',
+	//layout : 'fit',
 	region : 'center', // a center region is ALWAYS required for border layout
-	//deferredRender : false,
-	//activeTab : 0, // first tab initially active
-	closable: true,
-	items : [{
+	// deferredRender : false,
+	// activeTab : 0, // first tab initially active
+	closable : true,
+	items : [ {
+		xtype : 'toolbar',
+		items: {
+			xtype: 'button',
+			text: '保存'
+		}
+	}, {
 		xtype : 'form',
 		items : [ {
 			xtype : 'hidden',
@@ -17,10 +23,10 @@ Ext.define('CT.view.center.Edit', {
 		}, {
 			xtype : 'textarea',
 			name : 'tiContent',
-			//padding : '5 100 5 5',
+			// padding : '5 100 5 5',
 			width : '100%',
-			height : 898,
-			//fieldLabel : '输出信息'
+			height : 870,
+			// fieldLabel : '输出信息'
 		} ]
 	} ]
 })
