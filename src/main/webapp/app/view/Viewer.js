e0eb15d8ccea5739a5f548da1ffe93f8b8e08585
金为乐 <wl.jin@jiangtai.com>
2014-09-23 22:25:54 +0800

    tabchange

--- src/main/webapp/app/view/Viewer.js
+++ src/main/webapp/app/view/Viewer.js
@@ -1,20 +1,23 @@
 Ext.define('CT.view.Viewer', {
-    extend: 'Ext.tab.Panel',
-    alias: 'widget.viewer',
-    requires: [],
-    activeItem: 0,
-    margins: '5 0 5 5',
-    cls: 'preview',
-    deferredRender: false,
-    activeTab: 0,     // first tab initially active
-    items: [{
-        title: '使用说明',
-        autoScroll: true,
-        html: '欢迎使用代码生成工具，本系统正在开发中！'
-    }],
-    initComponent: function() {
-    	
-        
-        this.callParent(arguments);
-    }
+	extend : 'Ext.tab.Panel',
+	alias : 'widget.viewer',
+	requires : [],
+	activeItem : 0,
+	margins : '5 0 5 5',
+	cls : 'preview',
+	deferredRender : false,
+	activeTab : 0, // first tab initially active
+	items : [ {
+		title : '使用说明',
+		autoScroll : true,
+		html : '欢迎使用代码生成工具，本系统正在开发中！'
+	} ],
+	initComponent : function() {
+		this.callParent(arguments);
+	},
+	listeners : {
+		tabchange : function(tp, p) {
+			Ext.Msg.alert("提示", p.id);
+		}
+	}
 });
