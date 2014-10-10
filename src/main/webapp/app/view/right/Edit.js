b0637563971025904087fffd852f277debd632ef
金为乐 <wl.jin@jiangtai.com>
2014-10-10 12:57:40 +0800

    更新firefox不出现checkbox的bug

--- src/main/webapp/app/view/right/Edit.js
+++ src/main/webapp/app/view/right/Edit.js
@@ -16,29 +16,32 @@ Ext.define('CT.view.right.Edit', {
             }
         }
     },
-    /*selModel: Ext.create('Ext.selection.CheckboxModel',{
-    	listeners:{
-	    	select: function(model, selected, index, eOpts){
-	    		if(index < 0) return;
-	    		//ajax提交数据
-	    		var tc = Ext.ComponentQuery.query('#right_id');
-	    		//alert(tc[0].templateid);
-	    	},
-	    	deselect: function(model, selected, index, eOpts){
-	    		if(index < 0) return;
-	    		alert(2);
-	    		//ajax提交数据
-	    		var tc = Ext.ComponentQuery.query('#right_id');
-	    		//alert(tc[0].templateid);
-	    	}
-    	}
-    }),*/
     columns: [
               { dataIndex: 'diId', width: 18 },
-              { header: '数据库名',  dataIndex: 'diName', hidden: false },
+              { header: '数据库名',  dataIndex: 'diName', hidden: false, width: 65 },
               { header: '数据库类名', dataIndex: 'diClassname', hidden: false },
               { header: '连接字符串', dataIndex: 'diLinks', hidden: true },
               { header: '表sql', dataIndex: 'diTablesql', hidden: true },
               { header: '列sql', dataIndex: 'diColsql', hidden: true }
-    ]
+    ],
+    initComponent: function(){
+    	this.selModel = Ext.create('Ext.selection.CheckboxModel',{
+        	listeners:{
+    	    	select: function(model, selected, index, eOpts){
+    	    		if(index < 0) return;
+    	    		//ajax提交数据
+    	    		var tc = Ext.ComponentQuery.query('#right_id');
+    	    		//alert(tc[0].templateid);
+    	    	},
+    	    	deselect: function(model, selected, index, eOpts){
+    	    		if(index < 0) return;
+    	    		alert(2);
+    	    		//ajax提交数据
+    	    		var tc = Ext.ComponentQuery.query('#right_id');
+    	    		//alert(tc[0].templateid);
+    	    	}
+        	}
+        });
+    	this.callParent(arguments);
+    }
 });
