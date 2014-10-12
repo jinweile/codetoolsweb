dab2461ae8a37707049741b1bf1249839d35b906
金为乐 <wl.jin@jiangtai.com>
2014-10-12 12:45:35 +0800

    update

--- src/main/webapp/app/view/Right.js
+++ src/main/webapp/app/view/Right.js
@@ -2,7 +2,7 @@ Ext.define('CT.view.Right', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.right',
     requires: ['Ext.toolbar.Toolbar'],
-    title: '模板参数',
+    title: '模板-系统变量关联',
     collapsible: true,
     animCollapse: true,
     margins: '5 0 5 5',
